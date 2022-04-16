7baa0998d2c5d03c2209b3cf1d13f1cb546c76cb
Nathan Hendrix <nhendrix711@gmail.com>
2022-04-16 06:04:40 -0500

    1st

---
+++ src/test/java/Vc/CRMTestTest.java
@@ -0,0 +1,26 @@
+package Vc;
+
+import org.testng.annotations.Test;
+
+public class CRMTestTest {
+
+  @Test
+  public void loginTestTest() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void readConfigTest() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void startTest() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void tearDownTest() {
+    throw new RuntimeException("Test not implemented");
+  }
+}
